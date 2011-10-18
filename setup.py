40268772bc69a7a1c6d071f1f8c75b7d2ddf5caa
Manuel Francisco Naranjo <manuel@aircable.net>
2011-10-18 09:38:24 -0700

    initial pypi version

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 setup(
     name = "OpenProximity-External-Data",
-    version = "0.1",
+    version = "0.1.1",
     packages = ['openproximity'],
     include_package_data = True,
+    author = "Naranjo Manuel Francisco",
+    author_email = "manuel@aircable.net",
+    description = "This is OpenProximity external metadata package",
+    keywords = "openproximity proximity marketing bluetooth static",
+    url = "",
 )
